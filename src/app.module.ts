1ada12d4d28d43091d0bd27496ed2b9de51df250
gyomdyung <seastory624@gmail.com>
2022-09-16 16:32:57 +0900

    기본 설정 적용
    - HttpExceptionFilter
    - Response Interceptor
    - Validation Pipe
    - Logger
    - Swagger
    - 환경변수 Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmAsyncModuleOptions } from 'config/typeorm.config';
+import { typeOrmAsyncModuleOptions } from 'src/config/typeorm.config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: [`${__dirname}/../config/.config.env`],
+      envFilePath: '.env',
       isGlobal: true,
+      validationSchema: Joi.object({
+        MODE: Joi.string().valid('dev', 'prod').required(),
+        PORT: Joi.number().default(3000),
+      }),
     }),
     TypeOrmModule.forRootAsync(typeOrmAsyncModuleOptions),
   ],
