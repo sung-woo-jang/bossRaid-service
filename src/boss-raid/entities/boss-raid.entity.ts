4a3bdb731fdf7ddf2844a75c81694665c3a97a70
gyomdyung <seastory624@gmail.com>
2022-09-16 22:07:15 +0900

    Feat: BossRaid 엔티티 설정

--- src/boss-raid/entities/boss-raid.entity.ts
+++ src/boss-raid/entities/boss-raid.entity.ts
@@ -1,7 +1,26 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class BossRaid extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  level: number;
+
+  @Column({ default: 0 })
+  score: number;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  enterTime: Date;
+
+  @DeleteDateColumn({ type: 'timestamp' })
+  endTime: Date;
 }
