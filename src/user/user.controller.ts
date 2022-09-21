768665ab1ac07806fc34db747504318a7f40bb7f
gyomdyung <seastory624@gmail.com>
2022-09-21 09:35:03 +0900

    Docs: User API 스웨거 문서 Base

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,19 +6,40 @@ import {
   ParseIntPipe,
   Post,
 } from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
+@ApiTags('User API')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   // 유저 생성
+  @ApiOperation({
+    summary: '유저 생성 API',
+    description: '유저를 생성합니다.',
+  })
+  @ApiCreatedResponse({
+    description: '유저를 생성합니다.',
+  })
   @Post()
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
   }
 
+  @ApiOperation({
+    summary: '유저 조회 API',
+    description: '유저를 조회합니다.',
+  })
+  @ApiOkResponse({
+    description: '유저를 조회합니다.',
+  })
   @Get(':id')
   findUserWithRaidRecordById(@Param('id', ParseIntPipe) id: number) {
     return this.userService.findUserWithRaidRecordById(id);
